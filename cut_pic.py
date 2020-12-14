db82f8cee9c20468d00b9e09fe21ece15c58bdb6
konishi0125 <yumemita@gmail.com>
2020-12-14 17:08:32 +0900

    だいたい完成

--- cut_pic.py
+++ cut_pic.py
@@ -3,7 +3,8 @@ import cv2
 import numpy as np
 
 
-def onMouse( event, x, y, flag, params ):
+#切り出す座標を決定する
+def decide_coordinate( event, x, y, flag, params ):
     global x_, y_, start_x, start_y, end_x, end_y, click_flag
 
     wname, img = params
@@ -37,48 +38,45 @@ def onMouse( event, x, y, flag, params ):
             cv2.line( img2, ( x, y ), ( x_, y ), ( 255, 0, 0 ) )
             cv2.imshow( wname, img2 )
 
-    #if event == cv2.EVENT_LBUTTONDOWN:
-    #    print ( x, y )
 
+#切り出しのため画像のリサイズ
 def resize_pic(img, max_lng):
     orgHeight, orgWidth = img.shape[:2]
     rate = max_lng/max([orgHeight, orgWidth])
     rsz_img = cv2.resize(img, None, fx=rate, fy=rate)
 
-    return rsz_img
+    return rsz_img, rate
 
 
 ok_list = []
-ng_list = []  
+ng_list = []
 
-path = ""
-f_list = os.listdir(f'{path}/pic/')
+f_list = os.listdir(f"./input_pic/")
 
-f_num = 1
 for f in f_list:
     wname = "hoge"
     cv2.namedWindow( wname )
-    img = cv2.imread(f'{path}/pic/{f}')
-    img = resize_pic(img, 512)
+    img = cv2.imread(f"./input_pic/{f}")
+    #描画のため長辺512ピクセルに変換
+    img, rate = resize_pic(img, 512)
 
     x_ = 0
     y_ = 0
     click_flag = "first"
-    cv2.setMouseCallback( wname, onMouse, [ wname, img ] )
+    cv2.setMouseCallback( wname, decide_coordinate, [ wname, img ] )
     cv2.imshow( wname, img )
-
-    # exit when ESC-key is pressed
-    while cv2.waitKey( 0 ) != 10:
-        pass
+    cv2.waitKey( 0 )
 
     if(x_ != 0 or y_ != 0):
-        ok_path = f'{path}/ok/{f_num}.jpg'
-        cv2.imwrite(ok_path, img)
-        ok_list.append(f"{ok_path} 0 {start_x} {start_y} {end_x} {end_y}")
-        f_num += 1
+        ok_list.append(f"{f} {int(start_x/rate)} {int(start_y/rate)} {int(end_x/rate)} {int(end_y/rate)}")
+    else:
+        ng_list.append(f)
 
     cv2.destroyAllWindows()
 
-out_list = '\n'.join(ok_list)
-with open(f'{path}/ok/poslist.txt', 'wt') as f:
-    f.write(out_list)
+out_ok_list = "\n".join(ok_list)
+with open(f"./result/ok.txt", "wt") as f:
+    f.write(out_ok_list)
+out_ng_list = "\n".join(ng_list)
+with open(f"./result/ng.txt", "wt") as f:
+    f.write(out_ng_list)
